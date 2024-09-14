a15198665994bf4b19b041ca3182d179c7764636
Alexander McConville <alexander.mcconville@pubsubsandbox.onmicrosoft.com>
2024-09-13 19:48:35 -0700

    Adding trading strategy files to explore development implementation and testing in the future.

---
+++ src/strategies/momentum.py
@@ -0,0 +1,9 @@
+##### 
+#####
+#####
+##### Momentum strategy aims to exploit the continuation of existing market trends.
+##### It involves buying assets that have shown strong recent performance and selling those that have underperformed.
+##### This can be applied to various timeframes, intraday to long-term trends
+#####
+#####
+#####
